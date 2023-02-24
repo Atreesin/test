6042c974d9c6720bf0ab6054e30fc31c164507cd
Atreesin <82460657+Atreesin@users.noreply.github.com>
2023-02-24 14:51:18 -0300

    Update program.js

--- recursos/program.js
+++ recursos/program.js
@@ -1,4 +1,4 @@
-import * as informacion from '/informacion.json' assert {type: 'json'};
+import * as informacion from './informacion.json' assert {type: 'json'};
 
 const info = informacion.persona[0].foto_perfil.large
 document.getElementById("foto_perfil").setAttribute('src', '/recursos/'+info);
