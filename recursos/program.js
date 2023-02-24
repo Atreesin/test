1fd7d86b8b27a15a9f9135ea717726f0dfb3f33d
Atreesin <82460657+Atreesin@users.noreply.github.com>
2023-02-24 14:54:51 -0300

    Update program.js

--- recursos/program.js
+++ recursos/program.js
@@ -1,4 +1,4 @@
-import * as informacion from './informacion.json' assert {type: 'json'};
+import * as informacion from 'https://github.com/Atreesin/test/blob/main/recursos/informacion.json' assert {type: 'json'};
 
 const info = informacion.persona[0].foto_perfil.large
 document.getElementById("foto_perfil").setAttribute('src', '/recursos/'+info);
